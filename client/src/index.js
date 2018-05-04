5dde4ced50da655268ef8c1f1f4e39ec6b281e0e
LucasTaboada <ltlucastaboada@gmail.com>
2018-05-03 22:50:59 -0400

    remove index.css

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ApolloClient, ApolloProvider } from 'react-apollo';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
