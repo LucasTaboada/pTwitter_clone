4ccb45ce09755e2caae6f80f2c9715269ce81862
LucasTaboada <ltlucastaboada@gmail.com>
2018-05-04 22:52:06 -0400

    remove comment

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { ApolloClient, ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-//fsf
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
