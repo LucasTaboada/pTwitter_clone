91431bf0eaa170ee827f803abcf69df56b74bcb7
LucasTaboada <ltlucastaboada@gmail.com>
2018-05-16 22:59:20 -0400

    add nav style

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Nav from './Nav.js';
+import "../style/App.css";
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <dev className="App">
        <Nav/>
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
@@ -12,7 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </dev>
     );
   }
 }
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../style/Nav.css'
 
 const Nav = () => (
-    <div>
+    <div className="Nav">
         
     </div>
 );
