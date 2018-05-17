bce94f46e17f412c4db6fa1644d7b150e43df104
LucasTaboada <ltlucastaboada@gmail.com>
2018-05-16 22:44:14 -0400

    add Main.js

---
+++ client/src/components/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class main extends Component {
+    constructor(props){
+        super(props)
+        this.state = {tweet:""}
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default main;
